bc5e78d2d82b5e40591241e50a8b423b90e4daeb
Arkin Modi <16737086+arkinmodi@users.noreply.github.com>
2022-12-02 11:30:16 -0500

    day 2 part 2

--- day_02/day_02.py
+++ day_02/day_02.py
@@ -27,9 +27,9 @@ def day_02_part_1(filename: str) -> int:
 
     total_score = 0
     for round in input:
-        op_encrypted, my_encrypted = round.split(" ")
-        op_move, my_move = decrypt_opponent(
-            op_encrypted), decrypt_me(my_encrypted)
+        op_encrypted, me_encrypted = round.split(" ")
+        op_move = decrypt_opponent(op_encrypted)
+        my_move = decrypt_me(me_encrypted)
 
         score = 0
         if my_move == "rock":
@@ -53,11 +53,55 @@ def day_02_part_1(filename: str) -> int:
     return total_score
 
 
-# def day_02_part_2(filename: str) -> int:
-#     with open(filename) as f:
-#         input = [n.strip() for n in f]
+def day_02_part_2(filename: str) -> int:
+    with open(filename) as f:
+        input = [n.strip() for n in f]
 
-#     return 0
+    def decrypt_opponent(move: str) -> str:
+        match move:
+            case 'A': return "rock"
+            case 'B': return "paper"
+            case 'C': return "scissors"
+            case _: raise Exception("Invalid Move")
+
+    total_score = 0
+    for round in input:
+        op_encrypted, round_result = round.split(" ")
+        op_move = decrypt_opponent(op_encrypted)
+
+        score = 0
+        my_move = ""
+        if round_result == 'X':
+            if op_move == "rock":
+                my_move = "scissors"
+            elif op_move == "paper":
+                my_move = "rock"
+            else:
+                my_move = "paper"
+
+        elif round_result == 'Y':
+            score += 3
+            my_move = op_move
+
+        elif round_result == 'Z':
+            score += 6
+            if op_move == "rock":
+                my_move = "paper"
+            elif op_move == "paper":
+                my_move = "scissors"
+            else:
+                my_move = "rock"
+
+        if my_move == "rock":
+            score += 1
+        elif my_move == "paper":
+            score += 2
+        else:
+            score += 3
+
+        total_score += score
+
+    return total_score
 
 
 @pytest.mark.parametrize(
@@ -70,14 +114,14 @@ def test_day_02_part_1(filename: str, expected: int) -> None:
     assert day_02_part_1(filename) == expected
 
 
-# @pytest.mark.parametrize(
-#     ("filename", "expected"),
-#     (
-#         (EXAMPLE_TXT, 45000),
-#     )
-# )
-# def test_day_02_part_2(filename: str, expected: int) -> None:
-#     assert day_02_part_2(filename) == expected
+@pytest.mark.parametrize(
+    ("filename", "expected"),
+    (
+        (EXAMPLE_TXT, 12),
+    )
+)
+def test_day_02_part_2(filename: str, expected: int) -> None:
+    assert day_02_part_2(filename) == expected
 
 
 if __name__ == "__main__":
@@ -86,4 +130,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     print(f"Day 2 Part 1: {day_02_part_1(args.filename)}")
-    # print(f"Day 2 Part 2: {day_02_part_2(args.filename)}")
+    print(f"Day 2 Part 2: {day_02_part_2(args.filename)}")
